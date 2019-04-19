dde3aa3447cd0c33efd5f40ba88f6c0af8a174e4
mumu111111 <2646709035@qq.com>
2019-04-19 17:39:49 +0800

    mint-ui loadmore

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,14 +4,29 @@ import './index.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+import {Loadmore} from 'mint-ui'
+Vue.component(Loadmore.name, Loadmore);
+
 new Vue({
   el: '.container',
   data: {
-    lists: null
+    lists: null,
+    allLoaded: false  //是否已经全部加载完
   },
   created(){
     axios.post(url.hotLists).then(res=>{
       this.lists = res.data.lists
     })
+  },
+  methods: {
+    loadBottom() {
+      // 加载更多数据
+      //this.allLoaded = true;// 若数据已全部获取完毕
+      //this.$refs.loadmore.onTopLoaded();
+    },
+    handleBottomChange(status) {
+      //处理组件状态的变化  status 表示当前组件状态
+      console.log(status)
+    }
   }
 })
