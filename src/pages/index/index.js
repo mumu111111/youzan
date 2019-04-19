a28fdbda8754bd03db22360b4cab3e29afa11d95
mumu111111 <2646709035@qq.com>
2019-04-19 16:25:25 +0800

    推荐商品列表渲染

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,17 @@
-import '@/modules/css/common.css'
+import 'css/common.css'
 import './index.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+new Vue({
+  el: '.container',
+  data: {
+    lists: null
+  },
+  created(){
+    axios.post(url.hotLists).then(res=>{
+      this.lists = res.data.lists
+    })
+  }
+})
