5c2be148df6a4f76b34d8d0bb8fd0cc93ec9f248
mumu111111 <2646709035@qq.com>
2019-04-22 18:21:46 +0800

    加入goods search member cart pages

---
+++ src/pages/cart/cart.js
@@ -0,0 +1,11 @@
+import './cart_base.css'
+import './cart_trade.css'
+import './cart.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+
+
+new Vue({
+
+})
---
+++ src/pages/goods/goods.js
@@ -0,0 +1,9 @@
+
+  import 'goods_common.css'
+  import './goods_custom.css'
+  import './goods.css'
+  import './goods_theme.css'
+  import './goods_mars.cs'
+  import './goods_sku.css'
+
+
