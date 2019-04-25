62340050b3e0cd4b285c50e25700ca858763dd61
mumu111111 <2646709035@qq.com>
2019-04-25 17:17:37 +0800

    add 商品详情页

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -4,7 +4,8 @@ let url= {
     topList: '/category/topList',
     subList: '/category/subList',
     rankList:'/category/rank',
-    searchList: '/search/list'
+    searchList: '/search/list',
+    details: '/goods/details'
 }
 
 let host = 'http://rap2api.taobao.org/app/mock/7058'
--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -6,4 +6,50 @@
   import './goods_mars.css'
   import './goods_sku.css'
 
+  import Vue from 'vue'
+  import axios from 'axios'
+  import url from 'js/api.js'
+  import qs from 'qs'
+  import mixin from 'js/mixin.js'
+
+  let {id} = qs.parse(location.search.substr(1))
+  let detailTab = ['商品详情', '本店成交']
+
+  new Vue({
+    el: '#app',
+    data: {
+      details: null,
+      detailTab,
+      tabIndex: 0,
+      dealLists: null
+    },
+    created() {
+      this.getDetails()
+    },
+    methods: {
+      getDetails() {
+        axios.get('url.details', {id}).then(res=>{
+          this.details = res.data.data
+
+
+        })
+      },
+      changeTabIndex(index) {
+        this.tabIndex = index
+        if(index){
+          this.getDeal()
+       }
+        
+
+      },
+      getDeal() {
+        axios.post(url.deal, {id}).then(res=>{
+          this.dealLists = res.data.data.lists
+        })
+      } 
+    },
+    mixins: [mixin]
+
+
+  })
 
