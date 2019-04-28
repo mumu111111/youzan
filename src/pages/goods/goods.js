5846b2ae7556c003b550064a009c943aa045e468
mumu111111 <2646709035@qq.com>
2019-04-28 17:06:18 +0800

    add v-cloak

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -11,8 +11,9 @@
   import url from 'js/api.js'
   import qs from 'qs'
   import mixin from 'js/mixin.js'
+  import Swipe from 'components/Swipe.vue'
 
-  let {id} = qs.parse(location.search.substr(1))
+  let {id} = qs.parse(location.search.substr(1))  //解析地址中的参数id
   let detailTab = ['商品详情', '本店成交']
 
   new Vue({
@@ -21,32 +22,51 @@
       details: null,
       detailTab,
       tabIndex: 0,
-      dealLists: null
+      dealLists: null,
+      swipeList: null
     },
     created() {
       this.getDetails()
     },
     methods: {
       getDetails() {
-        axios.get('url.details', {id}).then(res=>{
-          this.details = res.data.data
-
-
+        axios.get(url.details, {id}).then(res=>{
+          //先处理原始数据，在赋值
+          let data = res.data.data
+          data.skuList.forEach(sku=>{
+            let lists = []
+            sku.lists.forEach(item=>{
+              lists.push({
+                active: false,
+                tag: item
+              })
+            })
+            sku.lists = lists
+          })
+          this.details = data
+          //轮播组件需要的图片
+          this.details.imgs.forEach(item=>{
+            this.swipeList.push({
+              clickUrl: '',
+              image: item
+            })
+          })
         })
       },
       changeTabIndex(index) {
         this.tabIndex = index
-        if(index){
+        if(index){//index!===0也就是为1时 焦点状态为“成交记录” 
           this.getDeal()
        }
-        
-
       },
       getDeal() {
         axios.post(url.deal, {id}).then(res=>{
           this.dealLists = res.data.data.lists
         })
       } 
+    },
+    components: {
+      Swipe
     },
     mixins: [mixin]
 
