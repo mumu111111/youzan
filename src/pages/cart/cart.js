e19f30e77ab2d364a185e58aa123aa0bbc266b1a
mumu111111 <2646709035@qq.com>
2019-04-28 23:24:27 +0800

    更新cart.js api.js

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -4,8 +4,26 @@ import './cart.css'
 
 import Vue from 'vue'
 import axios from 'axios'
+import mixin from 'js/mixin.js'
+import url from 'js/api.js'
+
 
 
 new Vue({
+    el: '.container',
+    data: {
+        lists: null
+    },
+    created() {
+
+    },
+    methods: {
+        getLists() {
+            axios.get(url.cartLists).then(res=>{
+                this.lists = res.data.cartList
+            })
+        }
+    },
+    mixins: [mixin]
 
 })
